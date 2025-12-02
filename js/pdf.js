4560f10297153d5146df52a44dff9d99d4ba820e
Pawel <klawinski.pawel@gmail.com>
2025-12-02 19:39:31 +0100

    Create pdf.js

---
+++ js/pdf.js
@@ -0,0 +1,15 @@
+// js/pdf.js
+export async function exportPdf(container, filename = 'raport.pdf') {
+  const opt = {
+    margin:       10,
+    filename:     filename,
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 2, useCORS: true },
+    jsPDF:        { unit: 'pt', format: 'a4', orientation: 'portrait' }
+  };
+  return new Promise((resolve, reject) => {
+    try {
+      html2pdf().set(opt).from(container).save().then(() => resolve(true)).catch(err => reject(err));
+    } catch (err) { reject(err); }
+  });
+}
